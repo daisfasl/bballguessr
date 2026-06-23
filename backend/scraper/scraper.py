35f4c3b027bab18336cbc71987de4dbfe8bcec69
Jacob Tan <jacobt07@students.cs.ubc.ca>
2026-06-23 14:58:18 +0800

    refactored scrape_site + now multiple return values

--- backend/scraper/scraper.py
+++ backend/scraper/scraper.py
@@ -2,20 +2,37 @@ import requests
 from bs4 import BeautifulSoup, Comment
 import pandas as pd
 import time
+import re
 
 def scrape_site(url):
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers = headers)
+    try:
+        headers = {"User-Agent": "Mozilla/5.0"}
+        response = requests.get(url, headers = headers)
 
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    table = soup.find('table', id = 'per_game_stats')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find('table', id = 'per_game_stats')
 
-    thead = table.find('thead') # type: ignore
+        # first return value, player stats in dict form
+        stats_dict = get_player_stats(table)
+        
+        # second return value, career length
+        career_length = get_career_length(table)
+
+        # third return value, image_url -- if not found, img = None
+        img = get_img_url(soup)
+
+    except Exception as e:
+        print(f"Unable to scrape url: {url}")
+        print(f"Error: {e}")
+        return
+    return (stats_dict, career_length, img)
+
+def get_player_stats(table):
+    thead = table.find('thead')
     headers = [th.getText() for th in thead.find_all('th') if th.getText() != ''] # type: ignore
     
-    tbody = table.find('tbody') # type: ignore
-    rows = tbody.find_all('tr') # type: ignore
+    tbody = table.find('tbody') 
+    rows = tbody.find_all('tr') 
     
     player_data = []
     for row in rows:
@@ -25,12 +42,28 @@ def scrape_site(url):
         player_data.append(row_data)
     
     df = pd.DataFrame(player_data, columns=headers)
+    stats_dict = df.to_dict()
 
-    return df
+    return stats_dict
 
-        
-        
+def get_career_length(table):
+    tfoot = table.find('tfoot') 
+    extracted_text = tfoot.find('th').getText()
+    career_length = re.search(r'\d+', extracted_text)
+    if not career_length:
+        raise ValueError("Unable to find career length")
+    else:
+        career_length = int(career_length.group())
+        return career_length
 
+def get_img_url(soup):
+    try:
+        div = soup.find('div', class_ = "media-item")
+        img = div.find('img')
+        img.get('src')
+    except Exception as e:
+        img = None
 url = 'https://www.basketball-reference.com/players/c/chambwi01.html'
-df = scrape_site(url)
-print(df.tail().to_dict())
+url2 = 'https://www.basketball-reference.com/players/a/anderbo01.html'
+url3 = 'https://www.basketball-reference.com/players/c/curryde01.html'
+print(scrape_site(url2))
