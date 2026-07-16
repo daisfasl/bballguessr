476220624dee308477417004cb530cb8faa55a92
Jacob Tan <jacobtan07@gmail.com>
2026-07-16 20:28:57 +0800

    completion: player_auto_complete()

--- backend/routes/players.py
+++ backend/routes/players.py
@@ -1,13 +1,21 @@
 from fastapi import APIRouter, Depends
 from backend.database import get_db
 from sqlalchemy.orm import Session
+from sqlalchemy import select
+from backend.models import Player
 
 router = APIRouter(prefix= "/api/players")
 
-@router.get("/{bball_ref_id}")
-def get_player(bball_ref_id):
-    pass
-
+# desc: return <=5 players w/ name AND id
 @router.get("/")
-def player_auto_complete(q: str| None = None):
-    pass
+def player_auto_complete(q: str| None = None, db: Session = Depends(get_db)):
+    if not q:
+        return None
+    else:
+        stmt = select(Player.name, Player.id).where(Player.name.istartswith(q)).limit(5) # not case sensitive (istartswith)
+        res = db.execute(stmt).all()
+        test = {} 
+        for name, id, in res:
+            test[name] = id
+        return test
+
