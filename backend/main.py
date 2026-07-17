c2f2ef6457ed836f55f3a32faeb4143fe6cf88ae
Jacob Tan <jacobtan07@gmail.com>
2026-07-17 16:29:36 +0800

    added middleware

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes.players import router as player_router
 from backend.routes.game import router as game_router
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods = ["*"],
+        allow_headers = ["*"]
+        )
 
 @app.get("/")
 async def root():
