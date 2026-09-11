aa79b292aeb01f365039f08bbd065eecf1ebe56f
Jacob Tan <jacobtan07@gmail.com>
2026-09-10 23:11:02 -0700

    legit fix

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ if not FRONTEND_URL:
     raise ValueError("No FRONTEND_URL in .env file")
 app = FastAPI()
 
-origins = FRONTEND_URL
+origins = [FRONTEND_URL]
 
 app.add_middleware(
         CORSMiddleware,
